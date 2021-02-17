78e6bb362ab8235a6a7577d0774dba701c1ee03d
Patrick Furtak <patrick@patrickfurtak.com>
2021-02-16 19:31:16 -0500

    Refactored code in assignment4 to use v-model to reduce the amount of code needed

--- section-1/assignment-4/app.js
+++ section-1/assignment-4/app.js
@@ -7,15 +7,8 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    updateClass(e) {
-      this.inputText = e.target.value;
-      console.log(this.inputText);
-    },
     toggleVisibility() {
       this.isVisible = !this.isVisible;
-    },
-    updateColor(e) {
-      this.bgColor = e.target.value;
     },
   },
 });
